d8a30714b2c6b2e297c8fcde11e057b90460b88e
Łukasz Korecki <lukasz@coffeesounds.com>
2011-07-21 21:58:43 +0100

    fail if not 1.9.2

--- lib/Flames.rb
+++ lib/Flames.rb
@@ -1,5 +1,7 @@
-require "Flames/version"
-
-module Flames
-  # Your code goes here...
+if RUBY_VERSION.split('.')[1].to_i < 9
+  puts "Flames requires ruby 1.9.2"
+  exit 1
 end
+
+require 'yaml'
+require "Flames/version"
