dd6b908cb33710cc804120a0d5e02156734f6902
Łukasz Korecki <lukasz@coffeesounds.com>
2011-07-24 14:45:13 +0100

    derp

--- lib/Flames/formatter.rb
+++ lib/Flames/formatter.rb
@@ -8,23 +8,25 @@ module Flames
       case m['type']
       when 'TimestampMessage'
         "< #{time.green} >"
+
       when 'TextMessage'
         "<#{m['user']['name'].yellow}> #{m['body']}"
+
       when 'PasteMessage'
         s = "<#{m['user']['name'].yellow}>"
         s << "\n"
         s << m['body']
-        s
+
       when 'KickMessage'
         s = "< "
         s << "#{m['user']['name']} left the room".dark_green
         s << " >"
-        s
+
       when 'EnterMessage'
         s = "< "
         s << "#{m['user']['name']} joined the room".dark_green
         s << " >"
-        s
+
       else
         "Unknown type! \n#{m.to_yaml}"
       end
