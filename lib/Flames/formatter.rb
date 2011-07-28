a17afa1c003e17f42ed7efd3d223c2a3eab46fd2
Łukasz Korecki <lukasz@coffeesounds.com>
2011-07-28 10:55:38 +0100

    colors, colors everywhere

--- lib/Flames/formatter.rb
+++ lib/Flames/formatter.rb
@@ -4,27 +4,34 @@ module Flames
       [:hour, :min, :sec].map { |m| Time.now.send m }.join ':'
     end
 
+    def random_color str
+      colors =  [:red, :green, :dark_green, :yellow, :blue, :dark_blue, :pur]
+      str.send colors.sample
+    end
+
     def format m
       case m['type']
       when 'TimestampMessage'
         "< #{time.green} >"
 
       when 'TextMessage'
-        "<#{m['user']['name'].yellow}> #{m['body']}"
+        "<#{random_color m['user']['name']}> #{m['body']}"
 
       when 'PasteMessage'
-        s = "<#{m['user']['name'].yellow}>"
-        s << "\n"
+        s = "<#{random_color m['user']['name']}>"
+        s << "\n\n"
+        s << "-"*20
         s << m['body']
+        s << "-"*20
 
       when 'KickMessage'
         s = "< "
-        s << "#{m['user']['name']} left the room".dark_green
+        s << "#{random_color m['user']['name']} left the room".dark_green
         s << " >"
 
       when 'EnterMessage'
         s = "< "
-        s << "#{m['user']['name']} joined the room".dark_green
+        s << "#{random_color m['user']['name']} joined the room".dark_green
         s << " >"
 
       else
